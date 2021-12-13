4729bf3b5d1557050a2dde21487ae13e9b6b5f1d
alexandrumalinescu <almalinescu@yahoo.com>
2021-12-13 16:54:27 +0200

    cookies set up

--- web server/routes/auth.js
+++ web server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const router = express.Router();
 
 router.post("/login", function (req, res, next) {
@@ -11,25 +12,54 @@ router.post("/login", function (req, res, next) {
   let users = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, "../users.json"), "utf8")
   );
-  let user = users.find(
+  let userFound = users.find(
     (user) =>
       user.email === req.body.email && user.password === req.body.password
   );
-  console.log(user);
-  res.json(user);
+  console.log(userFound);
+  if (userFound) {
+    console.log(req.body.email);
+    res.cookie("user", `${req.body.email}`);
+    res.cookie("password", `${req.body.password}`);
+    // res.send(JSON.stringify(req.cookies));
+    // console.log(res.cookie);
+    res.status(220).json(userFound);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+router.get("/login", (req, res) => {
+  // res.cookie("user", req.body.email);
+  // res.cookie("password", req.body.password);
+  res.send(JSON.stringify(req.cookies));
 });
 
 router.post("/register", function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
+
+  let user = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  let errors = validateUser(user);
+
+  if (errors.length > 0) {
+    res.status(400);
+    res.send(errors);
+  }
+
   let users = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, "../users.json"), "utf8")
   );
   users.push({
     id: users[users.length - 1].id + 1,
     name: "",
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
+    username: user.username,
+    email: user.email,
+    password: user.password,
     role: "user",
     loggedin: false,
     address: {
@@ -64,5 +94,19 @@ router.post("/register", function (req, res) {
   );
   res.send("Successfully registered");
 });
+
+function validateUser(user) {
+  let errors = [];
+
+  if (!user.email) {
+    errors.push("User email is required");
+  } else if (!user.password) {
+    errors.push("Password is required");
+  } else if (user.password.length < 6) {
+    errors.push("Password must have at least 6 characters");
+  }
+
+  return errors;
+}
 
 module.exports = router;
