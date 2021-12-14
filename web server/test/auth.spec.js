5d5f232592499561867714b65910f113bbe0eb45
alexandrumalinescu <almalinescu@yahoo.com>
2021-12-14 16:59:43 +0200

    'test'

--- web server/test/auth.spec.js
+++ web server/test/auth.spec.js
@@ -1,10 +1,20 @@
-const { expect } = require("chai");
-let chai = require("chai");
+const chai = require("chai");
 let chaiHttp = require("chai-http");
-
+const asert = chai.assert;
+const expect = chai.expect;
 const should = chai.should();
-const blablafunctie = require("../routes/auth");
 
 chai.use(chaiHttp);
 
-describe("Validation", function () {});
+let users = require("../app");
+
+describe("get User", function () {
+  it("get user ", () => {
+    chai
+      .request(users)
+      .get("/users/1")
+      .end((err, res) => {
+        expect(res.should.have.status(200));
+      });
+  });
+});
